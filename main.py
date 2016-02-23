6159423fe812df441f731bd088745d0b228ae52c
Ap0c <Ap0c@users.noreply.github.com>
2016-02-23 23:45:03 +0000

    Added routes and views for movie list and tv shows list.

--- main.py
+++ main.py
@@ -18,6 +18,22 @@ def index():
 	return render_template('index.html')
 
 
+@app.route('/movies')
+def movies():
+
+	"""Displays a list of movies."""
+
+	return render_template('movies.html')
+
+
+@app.route('/tv_shows')
+def tv_shows():
+
+	"""Displays a list of tv shows."""
+
+	return render_template('tv_shows.html')
+
+
 # ----- Main ----- #
 
 if __name__ == '__main__':
