1e606dbf65e3aba754e8de15fdef2c4f1cc74cd9
Ap0c <Ap0c@users.noreply.github.com>
2016-05-12 22:49:50 +0100

    Added some tests for the db module.

--- video_server/tests/test_db.py
+++ video_server/tests/test_db.py
@@ -1,7 +1,14 @@
 # ----- Imports ----- #
 
 import unittest
+import os
 from video_server.db import Database
+
+
+# ----- Setup ----- #
+
+SCHEMA_FILE = 'video_server/schema.sql'
+TEST_DB = 'test.db'
 
 
 # ----- Tests ----- #
@@ -12,6 +19,36 @@ class TestDb(unittest.TestCase):
 
 	def test_instantiate(self):
 
-		"""Makes sure the db instantiates without error."""
+		"""Db instantiates without error."""
 
-		db = Database('test.db')
+		db = Database(TEST_DB)
+		db = Database(TEST_DB, SCHEMA_FILE)
+
+	def test_query_select(self):
+
+		"""SELECT query runs without error."""
+
+		db = Database(TEST_DB, SCHEMA_FILE)
+
+		result = db.query('SELECT * FROM movies')
+		self.assertEqual(result, [])
+
+	def test_query_insert(self):
+
+		"""INSERT query runs without error."""
+
+		db = Database(TEST_DB, SCHEMA_FILE)
+
+		args = ('Dummy Movie', 'dummy/path')
+		result = db.query('INSERT INTO movies (name, path) VALUES (?, ?)', args)
+		self.assertEqual(result, 1)
+
+		row = db.query('SELECT * FROM movies')[0]
+		self.assertEqual(row['name'], args[0])
+		self.assertEqual(row['path'], args[1])
+
+	def tearDown(self):
+
+		"""Removes any instance of the test database."""
+
+		os.remove(TEST_DB)
