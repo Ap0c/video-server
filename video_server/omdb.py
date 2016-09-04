e33b5d4a9ebc96c760fcddf64ddd1275c405361d
Ap0c <Ap0c@users.noreply.github.com>
2016-09-04 18:15:23 +0100

    Added module for retrieving metadata from omdb, included stub methods.

---
+++ video_server/omdb.py
@@ -0,0 +1,18 @@
+# ----- Imports ----- #
+
+import omdb
+
+
+# ----- Functions ----- #
+
+def _movie_data(name):
+
+	"""Retrieves the info for a movie by name."""
+
+def _show_data(name):
+
+	"""Retrieves the info for a show by name."""
+
+def _ep_data(show, season, episode):
+
+	"""Retrieves the info for an episode."""
