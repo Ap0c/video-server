e1d1a7a2bc2bc20c5393c41c514ac7591c5d51d6
Ap0c <Ap0c@users.noreply.github.com>
2016-09-04 21:36:42 +0100

    Added methods for retrieving and storing media data and metadata in database. Fixed issue with missing fields in metadata.

--- video_server/omdb.py
+++ video_server/omdb.py
@@ -1,6 +1,7 @@
 # ----- Imports ----- #
 
 import omdb
+from .db import Database
 
 
 # ----- Setup ----- #
@@ -18,7 +19,7 @@ def _movie_data(name):
 	"""Retrieves the info for a movie by name."""
 
 	metadata = omdb.get(title=name)
-	return {f: metadata[f] for f in _MOVIE_FIELDS if f in metadata}
+	return {f: (metadata[f] if f in metadata else None) for f in _MOVIE_FIELDS}
 
 
 def _show_data(name):
@@ -26,7 +27,7 @@ def _show_data(name):
 	"""Retrieves the info for a show by name."""
 
 	metadata = omdb.get(title=name)
-	return {f: metadata[f] for f in _SHOW_FIELDS if f in metadata}
+	return {f: (metadata[f] if f in metadata else None) for f in _SHOW_FIELDS}
 
 
 def _ep_data(show=None, season=None, episode=None):
@@ -34,7 +35,7 @@ def _ep_data(show=None, season=None, episode=None):
 	"""Retrieves the info for an episode."""
 
 	metadata = omdb.get(title=show, season=season, episode=episode)
-	return {f: metadata[f] for f in _EP_FIELDS if f in metadata}
+	return {f: (metadata[f] if f in metadata else None) for f in _EP_FIELDS}
 
 
 def _lookup_movies(movies):
@@ -84,3 +85,45 @@ def _lookup_eps(episodes):
 		result.append(metadata)
 
 	return result
+
+
+def _scrape_movies(db):
+
+	"""Retrieves movies, looks up metadata, and stores it in the db."""
+
+	movies = db.query('SELECT id, name FROM movies')
+	db.many('INSERT INTO movie_metadata', movies)
+
+
+def _scrape_shows(db):
+
+	"""Retrieves shows, looks up metadata, and stores it in the db."""
+
+	shows = db.query('SELECT id, name FROM tv_shows')
+	db.many('INSERT INTO show_metadata', shows)
+
+
+def _scrape_eps(db):
+
+	"""Retrieves episodes, looks up metadata, and stores it in the db."""
+
+	episodes = db.query("""SELECT episodes.id AS id, episodes.number AS number,
+		episodes.season AS season, tv_shows.name AS show
+			FROM episodes, tv_shows
+			WHERE tv_shows.id = episodes.show""")
+	db.many('INSERT INTO episode_metadata', episodes)
+
+
+def lookup_media(db_file):
+
+	"""Retrieves the metadata for media in a database, and stores it."""
+
+	if db_file:
+
+		db = Database(db_file)
+		_scrape_movies(db)
+		_scrape_shows(db)
+		_scrape_eps(db)
+
+	else:
+		raise Exception('No db file given.')
