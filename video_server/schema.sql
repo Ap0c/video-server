bb7f4704899eac4441c04eeeee494c91ae14d06d
Ap0c <Ap0c@users.noreply.github.com>
2016-09-04 19:59:41 +0100

    Added test for movie lookup, and fixed bugs in database schema.

--- video_server/schema.sql
+++ video_server/schema.sql
@@ -16,13 +16,13 @@ CREATE TABLE IF NOT EXISTS media_locations
 CREATE TABLE IF NOT EXISTS movie_metadata
 	(id INTEGER PRIMARY KEY, poster_url TEXT, plot TEXT, runtime TEXT,
 		year TEXT, imdb_rating TEXT, actors TEXT, director TEXT,
-		FOREIGN KEY (id) REFERENCES movies)
+		FOREIGN KEY (id) REFERENCES movies(id));
 
 CREATE TABLE IF NOT EXISTS show_metadata
 	(id INTEGER PRIMARY KEY, poster_url TEXT, plot TEXT, year TEXT,
-		FOREIGN KEY (id) REFERENCES tv_shows)
+		FOREIGN KEY (id) REFERENCES tv_shows(id));
 
 CREATE TABLE IF NOT EXISTS episode_metadata
 	(id INTEGER PRIMARY KEY, poster_url TEXT, plot TEXT, runtime TEXT,
 		title TEXT,
-		FOREIGN KEY (id) REFERENCES episodes)
+		FOREIGN KEY (id) REFERENCES episodes(id));
--- video_server/tests/test_omdb.py
+++ video_server/tests/test_omdb.py
@@ -30,3 +30,13 @@ class TestOmdb(unittest.TestCase):
 
 		data = omdb._ep_data(show='Silicon Valley', season=1, episode=1)
 		self.assertIn('title', data)
+
+	def test_lookup_movies(self):
+
+		"""Makes sure list of movies is retrieved correctly."""
+
+		movies = [{'id': 1, 'name': 'Big Buck Bunny'}]
+		metadata = omdb._lookup_movies(movies)
+
+		self.assertEqual(metadata[0]['year'], '2008')
+		self.assertEqual(metadata[0]['id'], 1)
